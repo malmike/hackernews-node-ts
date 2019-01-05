e6769e00e7c1b9a9dd80c54abc694e9145e613c0
Michael Male <michael.male@andela.com>
2019-01-06 02:34:32 +0300

    simple query

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,30 @@ const {GraphQLServer} = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link]!
+  }
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for graphql'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a hackernews clone`
+    info: () => `This is the API of a hackernews clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
